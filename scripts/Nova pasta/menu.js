c1a0b6399403234828065e1dca5aedea4fb4ad6e
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-07-08 16:34:58 -0300

    .

---
+++ scripts/Nova pasta/menu.js
@@ -0,0 +1,15 @@
+function MudouTamanho(){
+            if(window.innerWidth >= 650){
+                itens.style.display = 'block'
+            }else{
+                itens.style.display = 'none'
+            }
+        }
+
+        function clickMenu(){
+            if(itens.style.display == 'block'){
+                itens.style.display = 'none'
+            } else{
+                itens.style.display = 'block'
+            }
+        }
