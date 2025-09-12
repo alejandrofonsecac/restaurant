4defd82aff2616324076ca3aa97a34dc9c692d87
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-09-12 17:54:45 -0300

    Revert "Add React Router"
    This reverts commit f5001790d0d8b272f6a48dac40f14ee14ca47228.

---
+++ pages/cardapio/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './src/App';
+import Carrinho from '../cart/Cart';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  },
+  {
+    path:'/carrinho',
+    element: <Carrinho/>,
+  }
+
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
