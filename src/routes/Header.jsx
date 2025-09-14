9f897d19e21739bb4f8fc1543e4ef90288253cb5
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-09-14 12:22:03 -0300

    Estilo atualizado

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -3,6 +3,7 @@ import style from '../../styles/header/header.module.css';
 import { clickMenu } from '../_components/clickMenu.js';
 
 
+
 function Header() {
   return (
     <header>
@@ -18,16 +19,16 @@ function Header() {
 
       <div className={style.headerIcons}>
         <div className={style.iconWrapper}>
-          <span className="material-icons">shopping_cart</span>
+          <span className="material-icons" style={{fontSize: 26}}>shopping_cart</span>
           <span className={style.cartCount}>0</span>
         </div>
 
         <div>
-          <span className="material-icons" onClick={clickMenu}>menu</span>
+          <span className="material-icons" onClick={clickMenu} style={{fontSize: 29}}>menu</span>
         </div>
       </div>
 
-      <menu id="itens" style={{ display: 'none' }} className={style.menuMobile}>
+      <menu id="itens" className={style.menuMobile}>
         <ul>
           <li><a href="#">Cardapio</a></li>
           <li><a href="#" style={{ color: 'black' }}>Sobre nós</a></li>
